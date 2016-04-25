7f93a94109c229bbf02eee3c0ab736e92fa31031
ghr <ghrwork@163.com>
2016-04-25 15:42:13 +0800

    Update index.js

--- javaScript/index.js
+++ javaScript/index.js
@@ -26,6 +26,7 @@ var api = [
         rightBar();
         search();
     }
+    var url='http://www.guohuiru.cn/';
 Myjax('https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx76173915e2553d72&redirect_uri='+url+'&response_type=code&scope=SCOPE&state=STATE#wechat_redirect ',function(data){
 alert(data);
 });
